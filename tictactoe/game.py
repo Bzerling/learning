8214206ebf91e371124a686a0184e5ff2dc2b1b1
Bzerling <erlingpat@gmail.com>
2022-02-22 18:09:28 +0500

    refactoring draw grid

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,23 +1,25 @@
-print('Enter cells: ')
-x = input()
-x = x.replace("_", " ", 9)
-print('---------')
-print('| ' + x[0], x[1], x[2] + ' |')
-print('| ' + x[3], x[4], x[5] + ' |')
-print('| ' + x[6], x[7], x[8] + ' |')
-print('---------')
+cells = input("Enter cells: ")
+cell_list = list(cells)
+
+
+def draw_grid(cell):
+    cell = cell.replace("_", " ", len(cell))
+    print("---------")
+    for i in range(0, len(cell), 3):
+        print('| ' + ' '.join(cell[i:i + 3]) + ' |')
+    print("---------")
 
 
 def analyze(): # analyze the game state
     win_X = ["X", "X", "X"]
     win_O = ["O", "O", "O"]
-    vert_win = [[x[0], x[3], x[6]], [x[1], x[4], x[7]], [x[2], x[5], x[8]]]
-    hor_win = [[x[0], x[1], x[2]], [x[3], x[4], x[5]], [x[6], x[7], x[8]]]  
-    diag_win = [[x[0], x[4], x[8]], [x[2], x[4], x[6]]]                     
-    if x.count('X') - x.count("O") > 1: # Detecting impossible state when X is 2 or more
+    vert_win = [[cells[0], cells[3], cells[6]], [cells[1], cells[4], cells[7]], [cells[2], cells[5], cells[8]]]
+    hor_win = [[cells[0], cells[1], cells[2]], [cells[3], cells[4], cells[5]], [cells[6], cells[7], cells[8]]]  
+    diag_win = [[cells[0], cells[4], cells[8]], [cells[2], cells[4], cells[6]]]                     
+    if cells.count('X') - cells.count("O") > 1: # Detecting impossible state when X is 2 or more
         print('Impossible')
         return
-    elif x.count('O') - x.count("X") > 1: # Detecting impossible state when O is 2 or more
+    elif cells.count('O') - cells.count("X") > 1: # Detecting impossible state when O is 2 or more
         print('Impossible')
         return
     if win_X in vert_win or win_X in hor_win or win_X in diag_win: # X won
@@ -27,7 +29,12 @@ def analyze(): # analyze the game state
             print('X wins')
     elif win_O in vert_win or win_O in hor_win or win_O in diag_win: # O won
         print('O wins')
-    elif x.count(' ') > 0: # if side has three in a row but the grid still has empty cell
+    elif cells.count(' ') > 0: # if side has three in a row but the grid still has empty cell
         print('Game not finished')
     else:
         print('Draw')
+
+
+
+draw_grid(cell=cells)
+analyze()
---
+++ training.py
@@ -0,0 +1,5 @@
+string = input().split()
+num = input()
+pos = []
+if num in enumerate(string):
+    print(pos)
